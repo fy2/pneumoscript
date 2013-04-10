7079ef7a88f65a82aab0aed0a174a7f30118ffb6
Feyruz Yalcin <fy2@sanger.ac.uk>
2013-04-10 11:29:16 +0100

    used in april 2014, 448 annotations

--- wrap.abacas.pl
+++ wrap.abacas.pl
@@ -8,4 +8,3 @@ mkdir "$ori.dir";
 chdir "$ori.dir";
 my $cmd = "bsub -o abacas.out -e abacas.err abacas.pl -p nucmer -m -b -o 'abacas' -q $ori -r /lustre/scratch108/pathogen/fy2/page/analysis_assembly/data/spn1041.reference.fasta";
 print `$cmd`;
-
--- wrap.psu.pl
+++ wrap.psu.pl
@@ -3,17 +3,21 @@
 use strict;
 use warnings;
 
+chdir($ARGV[0]);
+chdir('RATT');
 
-open(FH, '<'. $ARGV[0]) or die $!;
+open(FH, '<abacas.multifasta.and.contigsInBin.fasta') or die $!;
 
 my @ids;
 while(<FH>) {
     chomp;
-    if (/^>(\S+)\s/) {
-        push(@ids, 'ratt_out.' .  $1 . '.final.embl');
+    if (/^>(.+)/) {
+        my $id = $1;
+        $id =~ s/\|/_/g;
+        push(@ids, 'ratt_out.' .  $id . '.final.embl');
     }
 }
 close FH;
 my $str = join (" ", @ids);
-my $cmd = "psu_union.pl $str > psu.union.embl";
+my $cmd = "psu_union.pl $str > ../psu.union.embl";
 print `$cmd`;
