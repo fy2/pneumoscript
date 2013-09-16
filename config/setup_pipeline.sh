cbd772ad8f9ea596db3901a6f8ebd502801999d7
Feyruz Yalcin <fy2@sanger.ac.uk>
2013-09-16 15:04:55 +0100

    tidy paths

--- config/setup_pipeline.sh
+++ config/setup_pipeline.sh
@@ -2,13 +2,13 @@
 
 PERL5LIB=/nfs/pathogen002/fy2/perllib:$PERL5LIB
 PERL5LIB=/nfs/users/nfs_f/fy2/software/CoreGenomeUnofficial:$PERL5LIB
-PERL5LIB=/nfs/users/nfs_f/fy2/software/CoreGenomeUnofficial/perllibs/lib/perl5:$PERL5LIB
-PERL5LIB=/software/pathogen/internal/prod/lib:/software/pathogen/external/lib/lib/perl5:$PERL5LIB
+PERL5LIB=/nfs/users/nfs_f/fy2/software/perllibs/lib/perl5:$PERL5LIB
+PERL5LIB=/software/pathogen/internal/prod/lib:$PERL5LIB
+PERL5LIB=/software/pathogen/external/lib/lib/perl5:$PERL5LIB
 PERL5LIB=/software/badger/lib/perl5:$PERL5LIB
 export PERL5LIB
 
 PATH=/nfs/users/nfs_f/fy2/software/CoreGenomeUnofficial/script:$PATH
-PATH=/nfs/users/nfs_f/fy2/software/CoreGenomeUnofficial/script_custom:$PATH
 PATH=/nfs/users/nfs_f/fy2/local/bin:$PATH
 PATH=/nfs/users/nfs_f/fy2/software/ncbi-blast-2.2.28+/bin:$PATH
 PATH=/nfs/users/nfs_f/fy2/bin:$PATH
